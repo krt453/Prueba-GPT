5175965cc2a9316ed0ba9e60873c73ccffd23a0c
krt453 <94526880+krt453@users.noreply.github.com>
2025-07-01 00:07:45 +0200

    Add gunicorn and caching with Redis

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_caching import Cache
 
 app = Flask(__name__)
+app.config.from_mapping({
+    "CACHE_TYPE": "RedisCache",
+    "CACHE_REDIS_HOST": "localhost",
+    "CACHE_REDIS_PORT": 6379,
+})
+cache = Cache(app)
 
 @app.route('/')
+@cache.cached(timeout=60)
 def home():
     return '¡Bienvenido a Game Hub!'
 
